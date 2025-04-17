05885d9bc3f4827bb77777318a663e8702851d5f
rushalimoteria8 <84492105+rushalimoteria8@users.noreply.github.com>
2025-04-17 12:28:26 -0400

    Time: 35 ms (44.44%) | Memory: 30.9 MB (39.21%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    bool solve(int ind1, int ind2, string p, string s, vector<vector<int>> &dp){
+        if(ind2==0){
+            int k=ind1;
+            while(k--){
+                if(p[k]!='*') return dp[ind1][ind2]=false;
+            }
+
+            return dp[ind1][ind2]=true;
+        }
+
+        if(ind1==0){
+            return dp[ind1][ind2]=false;
+        }
+
+        if(p[ind1-1]==s[ind2-1]||p[ind1-1]=='?'){
+            return dp[ind1][ind2]=solve(ind1-1,ind2-1,p,s,dp);
+        }
+
+        if(p[ind1-1]=='*'){
+            //take it as empty string, or 1 char or 2 char...
+            return dp[ind1][ind2]=solve(ind1-1,ind2,p,s,dp)||solve(ind1,ind2-1,p,s,dp);
+        }
+
+        //mismatch
+        return dp[ind1][ind2]=false;
+    }
+
+
+    bool isMatch(string s, string p) {
+        
+        int n = p.size();
+        int m = s.size();
+
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        
+        // return solve(n,m,p,s,dp);
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+
+        //base case
+        for(int i=0;i<=n;i++){
+            int k=i;
+            dp[i][0]=true;
+            while(k--){
+                if(p[k]!='*'){ 
+                    dp[i][0]=false;
+                    break;
+                }
+            }
+        }
+
+        for(int j=1;j<=m;j++){
+            dp[0][j]=false;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[i-1]==s[j-1]||p[i-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p[i-1]=='*'){
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
